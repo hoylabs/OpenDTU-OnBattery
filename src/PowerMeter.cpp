260b3abe00858cd6f3b3d77646f16121d2ddaf74
qubeck <qubeck@qubeck.de>
2023-04-04 17:24:17 +0200

    minor cleanup/style changes and optim. PowerMeter

--- src/PowerMeter.cpp
+++ src/PowerMeter.cpp
@@ -36,20 +36,6 @@ void PowerMeterClass::init()
 
     CONFIG_T& config = Configuration.get();
 
-    if(config.PowerMeter_Source == 99){
-        #ifdef USE_SW_SERIAL
-        pinMode(SML_RX_PIN, INPUT);
-        inputSerial.begin(9600, SWSERIAL_8N1, SML_RX_PIN, -1, false, 128, 95);
-        inputSerial.enableRx(true);
-        inputSerial.enableTx(false);
-        #else
-        inputSerial.begin(9600, SERIAL_8N1, SML_RX_PIN, -1);
-        #endif        
-        inputSerial.flush();
-
-        _powerMeterOnlyTotalPowerAvailable = true;
-    }
-
     if (config.PowerMeter_Enabled && config.PowerMeter_Source == 0) {
         if (strlen(config.PowerMeter_MqttTopicPowerMeter1) > 0) {
             MqttSettings.subscribe(config.PowerMeter_MqttTopicPowerMeter1, 0, std::bind(&PowerMeterClass::onMqttMessage, this, _1, _2, _3, _4, _5, _6));
@@ -64,8 +50,22 @@ void PowerMeterClass::init()
         }
     }
 
-    if(config.PowerMeter_Source == 1 || config.PowerMeter_Source == 2){
+    if(config.PowerMeter_Source == 1 || config.PowerMeter_Source == 2) {
         sdm.begin();
+    }
+
+    if(config.PowerMeter_Source == 99) {
+        #ifdef USE_SW_SERIAL
+        pinMode(SML_RX_PIN, INPUT);
+        inputSerial.begin(9600, SWSERIAL_8N1, SML_RX_PIN, -1, false, 128, 95);
+        inputSerial.enableRx(true);
+        inputSerial.enableTx(false);
+        #else
+        inputSerial.begin(9600, SERIAL_8N1, SML_RX_PIN, -1);
+        #endif
+        inputSerial.flush();
+
+        _powerMeterOnlyTotalPowerAvailable = true;
     }
 
     mqttInitDone = true;
@@ -74,7 +74,7 @@ void PowerMeterClass::init()
 void PowerMeterClass::onMqttMessage(const espMqttClientTypes::MessageProperties& properties, const char* topic, const uint8_t* payload, size_t len, size_t index, size_t total)
 {
     CONFIG_T& config = Configuration.get();
-    if(config.PowerMeter_Enabled && config.PowerMeter_Source == 0){
+    if(config.PowerMeter_Enabled && config.PowerMeter_Source == 0) {
 
         if (strcmp(topic, config.PowerMeter_MqttTopicPowerMeter1) == 0) {
             _powerMeter1Power = std::stof(std::string(reinterpret_cast<const char*>(payload), (unsigned int)len));
@@ -94,16 +94,16 @@ void PowerMeterClass::onMqttMessage(const espMqttClientTypes::MessageProperties&
     _lastPowerMeterUpdate = millis();
 }
 
-float PowerMeterClass::getPowerTotal(){
+float PowerMeterClass::getPowerTotal() {
     return _powerMeterOnlyTotalPowerAvailable ? _powerMeterTotalPower : _powerMeter1Power + _powerMeter2Power + _powerMeter3Power;
 }
 
-uint32_t PowerMeterClass::getLastPowerMeterUpdate(){
+uint32_t PowerMeterClass::getLastPowerMeterUpdate() {
     return _lastPowerMeterUpdate;
 }
 
-void PowerMeterClass::mqtt(){
-    if (!MqttSettings.getConnected()){
+void PowerMeterClass::mqtt() {
+    if (!MqttSettings.getConnected()) {
         return;
     }else{
         String topic = "powermeter";
@@ -123,15 +123,15 @@ void PowerMeterClass::loop()
 {
     CONFIG_T& config = Configuration.get();
 
-    if (config.PowerMeter_Enabled && config.PowerMeter_Source == 99){
-        if (!smlReadLoop()){
+    if (config.PowerMeter_Enabled && config.PowerMeter_Source == 99) {
+        if (!smlReadLoop()) {
             return;
         }
     }
 
-    if(config.PowerMeter_Enabled && millis() - _lastPowerMeterUpdate >= (config.PowerMeter_Interval * 1000)){
+    if(config.PowerMeter_Enabled && millis() - _lastPowerMeterUpdate >= (config.PowerMeter_Interval * 1000)) {
         uint8_t _address = config.PowerMeter_SdmAddress;
-        if(config.PowerMeter_Source == 1){
+        if(config.PowerMeter_Source == 1) {
             _powerMeter1Power = static_cast<float>(sdm.readVal(SDM_PHASE_1_POWER, _address));
             _powerMeter2Power = 0.0;
             _powerMeter3Power = 0.0;
@@ -141,7 +141,7 @@ void PowerMeterClass::loop()
             _powerMeterImport = static_cast<float>(sdm.readVal(SDM_IMPORT_ACTIVE_ENERGY, _address));
             _powerMeterExport = static_cast<float>(sdm.readVal(SDM_EXPORT_ACTIVE_ENERGY, _address));
         }
-        if(config.PowerMeter_Source == 2){
+        if(config.PowerMeter_Source == 2) {
             _powerMeter1Power = static_cast<float>(sdm.readVal(SDM_PHASE_1_POWER, _address));
             _powerMeter2Power = static_cast<float>(sdm.readVal(SDM_PHASE_2_POWER, _address));
             _powerMeter3Power = static_cast<float>(sdm.readVal(SDM_PHASE_3_POWER, _address));
@@ -162,12 +162,12 @@ void PowerMeterClass::loop()
 
 bool PowerMeterClass::smlReadLoop()
 {
-    while (inputSerial.available()){
+    while (inputSerial.available()) {
         double readVal = 0;
         unsigned char smlCurrentChar = inputSerial.read();
         sml_states_t smlCurrentState = smlState(smlCurrentChar);
-        if (smlCurrentState == SML_LISTEND){
-            for(OBISHandler handler: smlHandlerList) {
+        if (smlCurrentState == SML_LISTEND) {
+            for(auto& handler: smlHandlerList) {
                 if (smlOBISCheck(handler.OBIS)) {
                     handler.Fn(readVal);
                     *handler.Arg = readVal;
