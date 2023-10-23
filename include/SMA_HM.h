aadfe5322715fef0504dab9e303768526f90da4f
Snoopy-HSS <hss@stapf.com>
2023-10-23 13:07:06 +0200

    modified:   include/SMA_HM.h 	modified:   src/SMA_HM.cpp

--- include/SMA_HM.h
+++ include/SMA_HM.h
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023 Holger-Steffen Stapf
+ */
 #pragma once
 
 #include <cstdint>
--- src/SMA_HM.cpp
+++ src/SMA_HM.cpp
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+/*
+ * Copyright (C) 2023 Holger-Steffen Stapf
+ */
 #include "SMA_HM.h"
 #include <Arduino.h>
 #include "Configuration.h"
