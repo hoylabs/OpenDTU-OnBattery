a80ee3f6ee5083d228242e8187e42c05ba57b182
Bernhard Kirchen <schlimmchen@posteo.net>
2024-09-05 21:31:25 +0200

    remove comments (wrong language, not very helpful)

--- pio-scripts/compile_webapp.py
+++ pio-scripts/compile_webapp.py
@@ -3,7 +3,6 @@ import hashlib
 import pickle
 import subprocess
 
-# Funktion zur Berechnung des Hash-Werts einer Datei
 def calculate_hash(file_path):
     hasher = hashlib.md5()
     with open(file_path, 'rb') as f:
@@ -11,17 +10,14 @@ def calculate_hash(file_path):
         hasher.update(buf)
     return hasher.hexdigest()
 
-# Funktion zur Überprüfung der Dateien in einem Ordner
 def check_files(directory, hash_file):
     file_hashes = {}
 
-    # Alle Dateien im Ordner und Unterordner durchsuchen
     for root, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(root, file)
             file_hashes[file_path] = calculate_hash(file_path)
 
-    # Laden der vorherigen Hash-Werte
     if os.path.exists(hash_file):
         with open(hash_file, 'rb') as f:
             old_file_hashes = pickle.load(f)
@@ -29,7 +25,6 @@ def check_files(directory, hash_file):
         old_file_hashes = {}
 
     changed = False
-    # Vergleich der aktuellen Hash-Werte mit den vorherigen
     for file_path, file_hash in file_hashes.items():
         if file_path not in old_file_hashes or old_file_hashes[file_path] != file_hash:
             changed = True
@@ -41,22 +36,17 @@ def check_files(directory, hash_file):
         print(f"webapp changed.")
         execute_command(directory)
 
-    # Speichern der aktuellen Hash-Werte
     with open(hash_file, 'wb') as f:
         pickle.dump(file_hashes, f)
 
-# Funktion zur Ausführung eines Befehls
 def execute_command(directory):
     print("Webapp changed, rebuilding...")
-    # change working directory
     print(f"Changing directory to: {directory}")
     os.chdir(directory)
-    # Run yarn install
     result = subprocess.run(["yarn", "install"], shell=True)
     if result.returncode != 0:
         print("Error during yarn install.")
         return
-    # Run yarn build
     result = subprocess.run(["yarn", "build"], shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         print("Error during yarn build:")
@@ -64,7 +54,6 @@ def execute_command(directory):
         print(result.stderr)
     else:
         print("Build completed successfully.")
-    # move back to old directory
     os.chdir("..")
 
 def main():
@@ -72,11 +61,9 @@ def main():
         print("INFO: not testing for up-to-date webapp artifacts when running as Github action")
         return 0
 
-    # Pfad zum Ordner und zur Hash-Datei
     directory = 'webapp'
     hash_file = ".webapp_hashes.pkl"
 
-    # Überprüfung der Dateien
     print("checke webapp")
     check_files(directory, hash_file)
 
