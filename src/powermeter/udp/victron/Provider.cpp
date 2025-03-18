9ff0f57906d7481f2ef06882b2a3ebbc0ed497c6
Bernhard Kirchen <schlimmchen@posteo.net>
2025-03-18 22:47:37 +0100

    victron power meter: implement sending request and parsing response

--- src/powermeter/udp/victron/Provider.cpp
+++ src/powermeter/udp/victron/Provider.cpp
@@ -10,6 +10,14 @@
 namespace PowerMeters::Udp::Victron {
 
 static constexpr unsigned int modbusPort = 502;  // local port to listen on
+
+// we only send one request which spans all registers we want to read
+static constexpr uint16_t sTransactionId = 0xDEAD; // arbitrary value
+static constexpr uint8_t sUnitId = 0x01;
+static constexpr uint8_t sFunctionCode = 0x03; // read holding registers
+static constexpr uint16_t sRegisterAddress = 0x3040;
+static constexpr uint16_t sRegisterCount = 0x004C;
+
 static WiFiUDP VictronUdp;
 
 Provider::Provider(PowerMeterUdpVictronConfig const& cfg)
@@ -36,9 +44,47 @@ void Provider::sendModbusRequest()
 
     if (currentMillis - _lastRequest < interval) { return; }
 
-    // TODO(schlimmchen): implement
+    std::vector<uint8_t> payload;
+
+    payload.push_back(sTransactionId >> 8);
+    payload.push_back(sTransactionId & 0xFF);
+
+    // protocol ID
+    payload.push_back(0x00);
+    payload.push_back(0x00);
+
+    // length
+    payload.push_back(0x00);
+    payload.push_back(0x06);
+
+    payload.push_back(sUnitId);
+    payload.push_back(sFunctionCode);
+    payload.push_back(sRegisterAddress >> 8);
+    payload.push_back(sRegisterAddress & 0xFF);
+    payload.push_back(sRegisterCount >> 8);
+    payload.push_back(sRegisterCount & 0xFF);
+
+    VictronUdp.beginPacket(_cfg.IpAddress, modbusPort);
+    VictronUdp.write(payload.data(), payload.size());
+    VictronUdp.endPacket();
 
     _lastRequest = currentMillis;
+}
+
+static float readInt16(uint8_t** buffer, uint8_t factor)
+{
+    uint8_t* p = *buffer;
+    int16_t value = (p[0] << 8) | p[1];
+    *buffer += 2;
+    return static_cast<float>(value) / factor;
+}
+
+static float readInt32(uint8_t** buffer, uint8_t factor)
+{
+    uint8_t* p = *buffer;
+    int32_t value = (p[0] << 24) | (p[1] << 16) | (p[2] << 8) | p[3];
+    *buffer += 4;
+    return static_cast<float>(value) / factor;
 }
 
 void Provider::parseModbusResponse()
@@ -46,10 +92,94 @@ void Provider::parseModbusResponse()
     int packetSize = VictronUdp.parsePacket();
     if (!packetSize) { return; }
 
-    uint8_t buffer[1024];
-    int rSize = VictronUdp.read(buffer, 1024);
+    uint8_t buffer[256];
+    VictronUdp.read(buffer, sizeof(buffer));
 
-    // TODO(schlimmchen): implement
+    uint8_t* p = buffer;
+
+    if (_verboseLogging) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] received %d bytes:", packetSize);
+
+        for (int i = 0; i < packetSize; i++) {
+            if (i % 16 == 0) {
+                MessageOutput.print("\r\n");
+            }
+            MessageOutput.printf("%02X ", buffer[i]);
+        }
+
+        MessageOutput.print("\r\n");
+    }
+
+    uint16_t transactionId = (p[0] << 8) | p[1];
+    p += 2;
+
+    if (transactionId != sTransactionId) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] invalid transaction ID: %04X\r\n", transactionId);
+        return;
+    }
+
+    uint16_t protocolId = (p[0] << 8) | p[1];
+    p += 2;
+
+    if (protocolId != 0x0000) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] invalid protocol ID: %04X\r\n", protocolId);
+        return;
+    }
+
+    uint16_t length = (p[0] << 8) | p[1];
+    p += 2;
+
+    uint16_t expectedLength = (sRegisterCount * 2) + 3;
+    if (length != expectedLength) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected length: %04X, "
+            "expected %04X\r\n", length, expectedLength);
+        return;
+    }
+
+    uint8_t unitId = p[0];
+    p += 1;
+
+    if (unitId != sUnitId) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected unit ID: %02X, "
+            "expected %02X\r\n", unitId, sUnitId);
+        return;
+    }
+
+    uint8_t functionCode = p[0];
+    p += 1;
+
+    if (functionCode != sFunctionCode) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected function code: %02X, "
+            "expected %02X\r\n", functionCode, sFunctionCode);
+        return;
+    }
+
+    uint8_t byteCount = p[0];
+    p += 1;
+
+    uint8_t expectedByteCount = sRegisterCount * 2;
+    if (byteCount != expectedByteCount) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected byte count: %02X, "
+            "expected %02X\r\n", byteCount, expectedByteCount);
+        return;
+    }
+
+    using Label = ::PowerMeters::DataPointLabel;
+    _dataCurrent.add<Label::VoltageL1>(readInt16(&p, 100)); // 0x3040
+    _dataCurrent.add<Label::CurrentL1>(readInt16(&p, 100)); // 0x3041
+    p += 12; // jump to register 0x3048
+    _dataCurrent.add<Label::VoltageL2>(readInt16(&p, 100)); // 0x3048
+    _dataCurrent.add<Label::CurrentL2>(readInt16(&p, 100)); // 0x3049
+    p += 12; // jump to register 0x3050
+    _dataCurrent.add<Label::VoltageL3>(readInt16(&p, 100)); // 0x3050
+    _dataCurrent.add<Label::CurrentL3>(readInt16(&p, 100)); // 0x3051
+    p += 92; // jump from 0x3052 to 0x3080 (0x2E registers = 92 bytes)
+    _dataCurrent.add<Label::PowerTotal>(readInt32(&p, 1)); // 0x3080
+    _dataCurrent.add<Label::PowerL1>(readInt32(&p, 1)); // 0x3082
+    p += 4; // jump to 0x3086
+    _dataCurrent.add<Label::PowerL2>(readInt32(&p, 1)); // 0x3086
+    p += 4; // jump to 0x308A
+    _dataCurrent.add<Label::PowerL3>(readInt32(&p, 1)); // 0x308A
 }
 
 void Provider::loop()
