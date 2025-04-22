27c7ee7f6a87b01c7bd0848ef604c4b06bd04170
Bernhard Kirchen <schlimmchen@posteo.net>
2025-04-22 21:17:31 +0200

    Vitron power meter: use heap-based buffer of dynamic size
    don't pretend to know the maximum size a UDP packet can have and do not
    allocate the buffer on the stack, as packets can be large.

--- src/powermeter/udp/victron/Provider.cpp
+++ src/powermeter/udp/victron/Provider.cpp
@@ -104,14 +104,10 @@ void Provider::parseModbusResponse()
     int packetSize = VictronUdp.parsePacket();
     if (0 == packetSize) { return; }
 
-    uint8_t buffer[256];
-    if (packetSize > sizeof(buffer)) {
-        MessageOutput.printf("[PowerMeters::Udp::Victron] packet too large: %d bytes\r\n", packetSize);
-        return;
-    }
-    VictronUdp.read(buffer, sizeof(buffer));
+    std::vector<uint8_t> buffer(packetSize);
+    VictronUdp.read(buffer.data(), packetSize);
 
-    uint8_t* p = buffer;
+    uint8_t const* p = buffer.data();
 
     if (_verboseLogging) {
         MessageOutput.printf("[PowerMeters::Udp::Victron] received %d bytes:", packetSize);
@@ -127,36 +123,38 @@ void Provider::parseModbusResponse()
     }
 
     uint16_t length = 0;
+    uint16_t protocolId = 0;
 
-    while (true) {
-        size_t processed = p - buffer;
-        size_t remaining = packetSize - processed;
-        if (remaining < 6) {
-            MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected end of packet\r\n");
-            return;
-        }
+    auto dataRemains = [&buffer, &p](size_t amount) -> bool {
+        return p - buffer.data() <= buffer.size() - amount;
+    };
 
-        uint16_t transactionId = (p[0] << 8) | p[1];
+    // even if length is 0, we expect at least 6 bytes (the header)
+    while (dataRemains(6)) {
+        uint16_t currentTransactionId = (p[0] << 8) | p[1];
         p += 2;
 
-        uint16_t protocolId = (p[0] << 8) | p[1];
+        protocolId = (p[0] << 8) | p[1];
         p += 2;
 
         length = (p[0] << 8) | p[1];
         p += 2;
 
-        if (transactionId != sTransactionId) {
-            MessageOutput.printf("[PowerMeters::Udp::Victron] skipping message with unexpected transaction ID: %04X\r\n", transactionId);
-            p += length;
-            continue;
-        }
-
-        if (protocolId != 0x0000) {
-            MessageOutput.printf("[PowerMeters::Udp::Victron] invalid protocol ID: %04X\r\n", protocolId);
+        if (!dataRemains(length)) {
+            MessageOutput.printf("[PowerMeters::Udp::Victron] unexpected end of packet\r\n");
             return;
         }
 
-        break;
+        if (currentTransactionId == sTransactionId) { break; }
+
+        MessageOutput.printf("[PowerMeters::Udp::Victron] skipping message "
+            "with unexpected transaction ID: %04X\r\n", currentTransactionId);
+        p += length;
+    }
+
+    if (protocolId != 0x0000) {
+        MessageOutput.printf("[PowerMeters::Udp::Victron] invalid protocol ID: %04X\r\n", protocolId);
+        return;
     }
 
     uint16_t expectedLength = (sRegisterCount * 2) + 3;
