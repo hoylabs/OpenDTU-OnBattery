51b989a98bad8839dae525234681e937e48504c7
Bernhard Kirchen <schlimmchen@posteo.net>
2025-05-19 19:58:25 +0200

    switch SerialPortManager to ESP_LOG macros

--- src/SerialPortManager.cpp
+++ src/SerialPortManager.cpp
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include "SerialPortManager.h"
-#include "MessageOutput.h"
+#include <HardwareSerial.h>
+#include <esp_log.h>
+
+#undef TAG
+static const char* TAG = "serialPortManager";
 
 SerialPortManagerClass SerialPortManager;
 
@@ -8,8 +12,7 @@ void SerialPortManagerClass::init()
 {
     if (ARDUINO_USB_CDC_ON_BOOT != 1) {
         _ports[0] = "Serial Console";
-        MessageOutput.println("[SerialPortManager] HW UART port 0 now in use "
-                "by 'Serial Console'");
+        ESP_LOGI(TAG, "HW UART port 0 now in use by 'Serial Console'");
     }
 }
 
@@ -17,21 +20,18 @@ std::optional<uint8_t> SerialPortManagerClass::allocatePort(std::string const& o
 {
     for (size_t i = 0; i < _ports.size(); ++i) {
         if (_ports[i] != "") {
-            MessageOutput.printf("[SerialPortManager] HW UART %d already "
-                    "in use by '%s'\r\n", i, _ports[i].c_str());
+            ESP_LOGI(TAG, "HW UART %d already in use by '%s'", i, _ports[i].c_str());
             continue;
         }
 
         _ports[i] = owner;
 
-        MessageOutput.printf("[SerialPortManager] HW UART %d now in use "
-                "by '%s'\r\n", i, owner.c_str());
+        ESP_LOGI(TAG, "HW UART %d now in use by '%s'", i, owner.c_str());
 
         return i;
     }
 
-    MessageOutput.printf("[SerialPortManager] Cannot assign another HW "
-            "UART port to '%s'\r\n", owner.c_str());
+    ESP_LOGE(TAG, "Cannot assign another HW UART port to '%s'", owner.c_str());
     _rejects.insert(owner);
     return std::nullopt;
 }
@@ -41,8 +41,7 @@ void SerialPortManagerClass::freePort(std::string const& owner)
     for (size_t i = 0; i < _ports.size(); ++i) {
         if (_ports[i] != owner) { continue; }
 
-        MessageOutput.printf("[SerialPortManager] Freeing HW UART %d, owner "
-                "was '%s'\r\n", i, owner.c_str());
+        ESP_LOGI(TAG, "Freeing HW UART %d, owner was '%s'", i, owner.c_str());
         _ports[i] = "";
     }
 }
