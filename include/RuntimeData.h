9e119250608ee84da4ceeee3308f4be198cf7896
SW-Nico <stbauer.privat@web.de>
2025-09-02 21:50:32 +0200

    Runtime: Base

---
+++ include/RuntimeData.h
@@ -0,0 +1,42 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+#pragma once
+
+#include <ArduinoJson.h>
+#include <TaskSchedulerDeclarations.h>
+#include <mutex>
+#include <atomic>
+
+
+class RuntimeClass {
+public:
+    explicit RuntimeClass(uint16_t version) : _writeVersion(version) {};
+    ~RuntimeClass() = default;
+    RuntimeClass(const RuntimeClass&) = delete;
+    RuntimeClass& operator=(const RuntimeClass&) = delete;
+    RuntimeClass(RuntimeClass&&) = delete;
+    RuntimeClass& operator=(RuntimeClass&&) = delete;
+
+    void init(Scheduler& scheduler);
+    bool read(void);
+    bool write(void);
+
+    uint16_t getWriteCount(void) const;
+    time_t getWriteEpochTime(void) const;
+    bool getReadState(void) const { return _readOK; }
+    bool getWriteState(void) const { return _writeOK; }
+    String getWriteCountAndTimeString(void) const;
+
+private:
+    void loop(void);
+    bool getWriteTrigger(void) const;
+
+    Task _loopTask;
+    std::atomic<bool> _readOK = false;      // true if the last read/write operation was successful
+    std::atomic<bool> _writeOK = false;     // true if the last read/write operation was successful
+    mutable std::mutex _mutex;              // to protect the shared data below
+    uint16_t _writeVersion = 0;             // shared data: version of the runtime data
+    uint16_t _writeCount = 0;               // shared data: number of times the data was written
+    time_t _writeEpoch = 0;                 // shared data: epoch time when the data was written
+};
+
+extern RuntimeClass RuntimeData;
---
+++ src/RuntimeData.cpp
@@ -0,0 +1,211 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+/* Runtime Data Management
+ *
+ * Read and write runtime data persistent on LittleFS
+ * - The data is stored in JSON format.
+ * - The data is written once a day at 00:05, during WebApp 'OTA firmware update' and 'Reset'.
+ * - The data is not written if last write was less then 1 hour ago.
+ * - Threadsave access to the data is provided by a mutex.
+ * Note: Additional runtime data must be added in the read() and write() methods.
+ *       To avoid reenter deadlocks, do not call write() or read() from a locally locked mutex to save local data!
+ *
+ * 2025.09.11 - 1.0 - first version
+ */
+
+#include <Utils.h>
+#include <LittleFS.h>
+#include <LogHelper.h>
+#include "RuntimeData.h"
+
+
+#undef TAG
+static const char* TAG = "configuration";
+static const char* SUBTAG = "runtime";
+
+
+constexpr const char* RUNTIME_FILENAME = "/runtime.json";   // filename of the runtime data file
+constexpr uint16_t RUNTIME_VERSION = 0;                     // version of the runtime data structure, prepared for future use
+
+
+RuntimeClass RuntimeData {RUNTIME_VERSION}; // singleton instance
+
+
+/*
+ * Init the runtime data loop task
+ */
+void RuntimeClass::init(Scheduler& scheduler)
+{
+    scheduler.addTask(_loopTask);
+    _loopTask.setCallback(std::bind(&RuntimeClass::loop, this));
+    _loopTask.setIterations(TASK_FOREVER);
+    _loopTask.setInterval(60 * 1000); // every minute
+    _loopTask.enable();
+}
+
+
+/*
+ * The runtime data loop is called every minute
+ */
+void RuntimeClass::loop(void)
+{
+    // check whether it is time to write the runtime data
+    if (getWriteTrigger()) { write(); }
+}
+
+
+/*
+ * Writes the runtime data to LittleFS file
+ */
+bool RuntimeClass::write(void)
+{
+    auto cleanExit = [this](const bool writeOk, const char* text) -> bool {
+        if (writeOk) {
+            DTU_LOGI("%s", text);
+        } else {
+            DTU_LOGE("%s", text);
+        }
+        _writeOK = writeOk;
+        return writeOk;
+    };
+
+    // we need the local time before we can write the runtime data
+    time_t nowEpoch;
+    if (!Utils::getEpoch(&nowEpoch, 5)) { return cleanExit(true, "Local time not available, skipping write"); }
+
+    JsonDocument doc;
+    { // mutex is automatically released when lock goes out of this scope
+        std::lock_guard<std::mutex> lock(_mutex);
+
+        // we do not write more than once in a hour
+        if ((_writeEpoch != 0) && (difftime(nowEpoch, _writeEpoch) < 60 * 60)) {
+            return cleanExit(true, "Last write less than 1 hour ago, skipping write");
+        }
+
+        _writeEpoch = nowEpoch;
+        _writeCount++;
+        JsonObject info = doc["info"].to<JsonObject>();
+        info["version"] = _writeVersion;
+        info["save_count"] = _writeCount;
+        info["save_epoch"] = _writeEpoch;
+    } // mutex is automatically released when lock goes out of this scope
+
+    // Insert additional runtime data here and protect the shared data with a local mutex
+
+
+    if (!Utils::checkJsonAlloc(doc, __FUNCTION__, __LINE__)) {
+        return cleanExit(false, "JSON alloc fault, skipping write");
+    }
+
+    File fRuntime = LittleFS.open(RUNTIME_FILENAME, "w");
+    if (!fRuntime) { return cleanExit(false, "Failed to open file for writing"); }
+
+    if (serializeJson(doc, fRuntime) == 0) {
+        fRuntime.close();
+        return cleanExit(false, "Failed to serialize to file");
+    }
+
+    fRuntime.close();
+    return cleanExit(true, "Written to file");
+}
+
+
+/*
+ * Read the runtime data from LittleFS file
+ */
+bool RuntimeClass::read(void)
+{
+    bool readOk = false;
+    JsonDocument doc;
+
+    // Note: We do not exit on read or allocation errors. In that case we need the default values
+    File fRuntime = LittleFS.open(RUNTIME_FILENAME, "r", false);
+    if (fRuntime) {
+        Utils::skipBom(fRuntime);
+        DeserializationError error = deserializeJson(doc, fRuntime);
+        if (!error && Utils::checkJsonAlloc(doc, __FUNCTION__, __LINE__)) {
+            readOk = true; // success of reading the runtime data
+        }
+    }
+
+    JsonObject info = doc["info"];
+    { // mutex is automatically released when lock goes out of this scope
+        std::lock_guard<std::mutex> lock(_mutex);
+        _writeVersion = info["version"] | RUNTIME_VERSION;
+        _writeCount = info["save_count"] | 0U;
+        _writeEpoch = info["save_epoch"] | 0U;
+    } // mutex is automatically released when lock goes out of this scope
+
+    // deserialize additional runtime data here, prepare default values and protect the shared data with a local mutex
+
+
+    if (fRuntime) { fRuntime.close(); }
+    if (readOk) {
+        DTU_LOGI("Read successfully");
+    } else {
+        DTU_LOGE("Read fault, using default values");
+    }
+    _readOK = readOk;
+    return readOk;
+}
+
+
+/*
+ * Get the write counter
+ */
+uint16_t RuntimeClass::getWriteCount(void) const
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _writeCount;
+}
+
+
+/*
+ * Get the write epoch time
+ */
+time_t RuntimeClass::getWriteEpochTime(void) const
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    return _writeEpoch;
+}
+
+
+/*
+ * Get the write count and time as string
+ * Format: "<count> / <dd>-<mon> <hh>:<mm>"
+ * If epoch time and local time is not available the time is replaced by "no time"
+ */
+String RuntimeClass::getWriteCountAndTimeString(void) const
+{
+    std::lock_guard<std::mutex> lock(_mutex);
+    char buf[32] = "";
+    struct tm time;
+    if ((_writeEpoch != 0) && (getLocalTime(&time, 5))) {
+        localtime_r(&_writeEpoch, &time);
+        strftime(buf, sizeof(buf), " / %d-%h %R", &time);
+    } else {
+        snprintf(buf, sizeof(buf), " / no time");
+    }
+    String ctString = String(_writeCount) + String(buf);
+    return ctString;
+}
+
+
+/*
+ * Returns true at the daily trigger time at 00:05
+ */
+bool RuntimeClass::getWriteTrigger(void) const {
+    static bool lastTrigger = false;
+    struct tm actTime;
+    if (getLocalTime(&actTime, 5)) {
+        if ((actTime.tm_hour == 0) && (actTime.tm_min >= 5) && (actTime.tm_min <= 10)) {
+            if (lastTrigger == false) {
+                lastTrigger = true;
+                return true;
+            }
+        } else {
+            lastTrigger = false;
+        }
+    }
+    return false;
+}
