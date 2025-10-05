071dc68b66d7692de62d2acec989d42640b5f86b
Andreas Böhm <andreas@boehm.cx>
2025-10-05 15:40:38 +0200

    adjust JSON to include provider

--- src/gridcharger/trucki/Stats.cpp
+++ src/gridcharger/trucki/Stats.cpp
@@ -1,4 +1,5 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
+#include <Configuration.h>
 #include <MqttSettings.h>
 #include <gridcharger/trucki/Stats.h>
 
@@ -23,37 +24,35 @@ void Stats::getLiveViewData(JsonVariant& root) const
 {
     root["vendorName"] = "Trucki";
     root["productName"] = "T2HG/T2MG";
-    root["showSettings"] = false;
+    root["provider"] = GridChargerProviderType::TRUCKI;
 
     root["dataAge"] = millis() - getLastUpdate();
     root["reachable"] = root["dataAge"] < 10000;
 
-    using Label = GridChargers::Trucki::DataPointLabel;
-
-    auto oDcPower = _dataPoints.get<Label::DcPower>();
-    auto oDcCurrent = _dataPoints.get<Label::DcCurrent>();
+    auto oDcPower = _dataPoints.get<DataPointLabel::DcPower>();
+    auto oDcCurrent = _dataPoints.get<DataPointLabel::DcCurrent>();
     root["producing"] = oDcPower.value_or(0) > 10 && oDcCurrent.value_or(0) > 0.1;
 
-    addStringInSection<Label::ZEPC>(root, "device", "zepc", false);
-    addStringInSection<Label::State>(root, "device", "state", false);
-    addValueInSection<Label::Temperature>(root, "device", "temp", 1);
-    addValueInSection<Label::Efficiency>(root, "device", "efficiency", 0);
-    addValueInSection<Label::DayEnergy>(root, "device", "yieldDay", 2);
-    addValueInSection<Label::TotalEnergy>(root, "device", "yieldTotal", 2);
+    addStringInSection<DataPointLabel::ZEPC>(root, "device", "zepc", false);
+    addStringInSection<DataPointLabel::State>(root, "device", "state", false);
+    addValueInSection<DataPointLabel::Temperature>(root, "device", "temp", 1);
+    addValueInSection<DataPointLabel::Efficiency>(root, "device", "efficiency", 0);
+    addValueInSection<DataPointLabel::DayEnergy>(root, "device", "yieldDay", 2);
+    addValueInSection<DataPointLabel::TotalEnergy>(root, "device", "yieldTotal", 2);
 
-    addValueInSection<Label::AcVoltage>(root, "input", "voltage", 1);
-    addValueInSection<Label::AcPower>(root, "input", "power", 1);
-    addValueInSection<Label::AcPowerSetpoint>(root, "input", "powerSetpoint", 1);
-    addValueInSection<Label::MinAcPower>(root, "input", "minPower", 0);
-    addValueInSection<Label::MaxAcPower>(root, "input", "maxPower", 0);
+    addValueInSection<DataPointLabel::AcVoltage>(root, "input", "voltage", 1);
+    addValueInSection<DataPointLabel::AcPower>(root, "input", "power", 1);
+    addValueInSection<DataPointLabel::AcPowerSetpoint>(root, "input", "powerSetpoint", 1);
+    addValueInSection<DataPointLabel::MinAcPower>(root, "input", "minPower", 0);
+    addValueInSection<DataPointLabel::MaxAcPower>(root, "input", "maxPower", 0);
 
-    addValueInSection<Label::DcVoltage>(root, "output", "voltage", 2);
-    addValueInSection<Label::DcVoltageSetpoint>(root, "output", "voltageSetpoint", 2);
-    addValueInSection<Label::DcPower>(root, "output", "power", 1);
-    addValueInSection<Label::DcCurrent>(root, "output", "current", 2);
+    addValueInSection<DataPointLabel::DcVoltage>(root, "output", "voltage", 2);
+    addValueInSection<DataPointLabel::DcVoltageSetpoint>(root, "output", "voltageSetpoint", 2);
+    addValueInSection<DataPointLabel::DcPower>(root, "output", "power", 1);
+    addValueInSection<DataPointLabel::DcCurrent>(root, "output", "current", 2);
 
-    addValueInSection<Label::DcVoltageOffline>(root, "settings", "offlineVoltage", 0);
-    addValueInSection<Label::DcCurrentOffline>(root, "settings", "offlineCurrent", 0);
+    addValueInSection<DataPointLabel::DcVoltageOffline>(root, "settings", "offlineVoltage", 0);
+    addValueInSection<DataPointLabel::DcCurrentOffline>(root, "settings", "offlineCurrent", 0);
 }
 
 }; // namespace GridChargers::Trucki
