10c327f36cf4593cf20c6d426c93ab619d0f9657
SW-Nico <stbauer.privat@web.de>
2025-10-09 13:57:40 +0200

    Runtime: fix read() timing
    read() must be done after all other components have been initialized

--- src/main.cpp
+++ src/main.cpp
@@ -146,14 +146,17 @@ void setup()
     Datastore.init(scheduler);
     RestartHelper.init(scheduler);
 
-    // OpenDTU-OnBattery-specific initializations go below
-    RuntimeData.init(scheduler);
-    RuntimeData.read(); // make runtime data available as early as possible
+    // OpenDTU-OnBattery-specific initializations go between here...
     SolarCharger.init(scheduler);
     PowerMeter.init(scheduler);
     PowerLimiter.init(scheduler);
     GridCharger.init(scheduler);
     Battery.init(scheduler);
+    // ... and here (before RuntimeData)
+
+    // Must be done after all other components have been initialized
+    RuntimeData.init(scheduler);
+    RuntimeData.read();
 
     ESP_LOGI(TAG, "Startup complete");
 }
