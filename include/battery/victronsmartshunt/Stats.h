3dccb1fec8a43177eeba33d1bd3e0b0485934d93
SW-Nico <stbauer.privat@web.de>
2026-03-28 18:08:07 +0100

    VeFrameError: WebApp LiveView
    - add error counter to  MPPT
    - add error counter to  Smart Shunt

--- include/battery/victronsmartshunt/Stats.h
+++ include/battery/victronsmartshunt/Stats.h
@@ -29,6 +29,7 @@ private:
     bool _alarmLowSOC;
     bool _alarmLowTemperature;
     bool _alarmHighTemperature;
+    float _transmissionErrors;
 };
 
 } // namespace Batteries::VictronSmartShunt
--- src/solarcharger/victron/Stats.cpp
+++ src/solarcharger/victron/Stats.cpp
@@ -251,6 +251,9 @@ void Stats::populateJsonWithInstanceStats(const JsonObject &root, const VeDirect
         device["MpptTemperature"]["u"] = "°C";
         device["MpptTemperature"]["d"] = "1";
     }
+    device["MpptTransmitError"]["v"] = mpptData.transmissionErrors_Day;
+    device["MpptTransmitError"]["u"] = "1/d";
+    device["MpptTransmitError"]["d"] = "1";
 
     if (mpptData.ChargeCurrentLimit.first > 0) {
         if (mpptData.ChargeCurrentLimit.second == UINT16_MAX) {
